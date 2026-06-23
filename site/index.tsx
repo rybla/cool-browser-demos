df3c986df18ade87cfbf681bee8b3bf112782f37
Henry Blanchette <henry@rybl.net>
2026-06-23 00:35:59 -0400

    made index better

--- site/index.tsx
+++ site/index.tsx
@@ -1,13 +1,13 @@
 import { get_pages } from "./index.macro" with { type: "macro" };
 
-const root = document.getElementById("root")!;
-
 const pages = get_pages();
+
+const pagesList = document.getElementById("pages-list")!;
 
 for (const page of pages) {
   const item = document.createElement("div");
   item.classList.add("page-item");
-  root.appendChild(item);
+  pagesList.appendChild(item);
 
   const anchor = document.createElement("a");
   item.appendChild(anchor);
