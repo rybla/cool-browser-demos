17d142fb1027f682be49ce8d16bd9f9c89ab225b
Henry Blanchette <henry@rybl.net>
2026-06-25 10:26:24 -0400

    docstring

--- site/index.macro.ts
+++ site/index.macro.ts
@@ -1,6 +1,10 @@
 import fs from "fs";
 import path from "path";
 
+/**
+ * Each directory in `site/` is treated as a page route route. This function
+ * lists all of these page routes.
+ */
 export function get_pages(): { name: string; path: string }[] {
   return fs
     .readdirSync("site")
